66632a6d587f4db3fed08e6b431bd931a77c40bd
Adem-green <67866908+Adem-green@users.noreply.github.com>
2021-05-23 00:02:02 +0500

    Update index.php

--- index.php
+++ index.php
@@ -24,14 +24,4 @@ $b = count($text_link);
     </div>
 </body>
 </html>
-//CREATE TABLE entries
-(
-    id INT PRIMARY KEY,
-    parent_id INT,
-    name VARCHAR(20),
-    description TEXT,
-    created_at DATA TIME,
-    updated_at DATA TIME,
-    deleted_at DATA TIME
-)
 
